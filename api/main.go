a0a68b9854859640ceb0a3274c9123acf8989adf
DarrenAnthonyBeltham <darrenanthonybeltham@gmail.com>
2025-09-09 15:46:13 +0700

    update

--- api/main.go
+++ api/main.go
@@ -1,34 +1,10 @@
 package handler
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-	"sync"
-
-	"cinelume/api/pkg/database"
-	"cinelume/api/pkg/routes"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
-
-var (
-	router *gin.Engine
-	once   sync.Once
-)
-
-func setupRouter() *gin.Engine {
-	if err := godotenv.Load(); err != nil {
-		log.Println("Warning: .env file not found")
-	}
-	db := database.Connect()
-	r := routes.SetupRoutes(db)
-	return r
-}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	once.Do(func() {
-		router = setupRouter()
-	})
-	router.ServeHTTP(w, r)
+	fmt.Fprintf(w, "Go API is running!")
 }
